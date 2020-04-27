c8deb854880206a6f35134a742fe5df2a248dd05
s.ikeda <s.ikeda107@gmail.com>
2020-04-28 00:45:16 +0900

    add: SlotGame logic

--- src/js/main.js
+++ src/js/main.js
@@ -7,11 +7,26 @@
       section.classList.add('panel');
 
       this.img = document.createElement('img');
-      this.img.src = 'img/seven.png';
+      this.img.src = this.getRadomImage();
+
+      this.timeoutId = undefined;
 
       this.stop = document.createElement('div');
       this.stop.textContent = 'STOP';
-      this.stop.classList.add('stop');
+      this.stop.classList.add('stop','inactive');
+      this.stop.addEventListener('click', () => {
+        if (this.stop.classList.contains('inactive')) {
+          return;
+        }
+        this.stop.classList.add('inactive');
+        clearTimeout(this.timeoutId);
+        panelLeft--;
+        if (panelLeft === 0) {
+          spin.classList.remove('inactive');
+          panelLeft = 3;
+          checkResult();
+        }
+      });
 
       section.appendChild(this.img);
       section.appendChild(this.stop);
@@ -21,7 +36,60 @@
         main.appendChild(section);
       }
     }
+
+    getRadomImage() {
+      const images = ['img/seven.png', 'img/bell.png', 'img/cherry.png'];
+      return images[Math.floor(Math.random() * images.length)];
+    }
+
+    spin() {
+      this.img.src = this.getRadomImage();
+      this.timeoutId = setTimeout(() => {
+        this.spin();
+      }, 50);
+    }
+
+    isUnmatched(p1, p2) {
+      return this.img.src != p1.img.src && this.img.src != p2.img.src;
+    }
+
+    unmatch() {
+      this.img.classList.add('unmatched');
+    }
+
+    activate(){
+      this.img.classList.remove('unmatched');
+      this.stop.classList.remove('inactive');
+    }
+  }
+
+  function checkResult() {
+    if (panels[0].isUnmatched(panels[1], panels[2])) {
+      panels[0].unmatch();
+    }
+    if (panels[1].isUnmatched(panels[0], panels[2])) {
+      panels[1].unmatch();
+    }
+    if (panels[2].isUnmatched(panels[0], panels[1])) {
+      panels[2].unmatch();
+    }
   }
 
   const panels = [new Panel(), new Panel(), new Panel()];
+
+  let panelLeft = 3;
+
+  const spin = document.getElementById('spin');
+  if (spin) {
+    spin.addEventListener('click', () => {
+      if (spin.classList.contains('inactive')) {
+        return;
+      }
+      spin.classList.add('inactive');
+      panels.forEach((panel) => {
+        panel.activate();
+        panel.spin();
+      });
+    });
+  }
 }
