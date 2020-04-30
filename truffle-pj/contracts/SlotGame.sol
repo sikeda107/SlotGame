4d34ce41f390d3fbba6a4dffbee53d33868f7bd8
s.ikeda <s.ikeda107@gmail.com>
2020-04-30 12:49:38 +0900

    add: Ether受け取り関数追加とコメント追加

--- truffle-pj/contracts/SlotGame.sol
+++ truffle-pj/contracts/SlotGame.sol
@@ -2,6 +2,10 @@ pragma solidity >=0.6.6 <0.7.0;
 pragma experimental ABIEncoderV2;
 
 
+/// @title Recorder the Results of Playing Slot Game
+/// @author s.ikeda107
+/// @notice You can record the results of playing the game.
+/// @dev This contract use ABIEncoderV2. This contract has been programmed with sol version 0.6.6 in mind.
 contract SlotGame {
     struct User {
         string name;
@@ -17,12 +21,22 @@ contract SlotGame {
         usersCount = 0;
     }
 
+    event Received(address, uint256);
+
+    /// @notice Receive Ether Function
+    receive() external payable {
+        emit Received(msg.sender, msg.value);
+    }
+
     event createdUser(
         string createdUserName,
         uint256 createdUserCreatedTime,
         uint256 createdUsersCount
     );
 
+    /// @notice Create a User for each address when not already created.
+    /// @dev This function throw event after creating user.
+    /// @param _name user name
     function createUser(string memory _name) public {
         // 引数なしまたは、作成済みでない
         require(
@@ -42,6 +56,8 @@ contract SlotGame {
         );
     }
 
+    /// @notice Get your user information
+    /// @return the current user of User type.
     function getMyUserInfo() public view returns (User memory) {
         // 作成済みであること
         require(
@@ -51,6 +67,7 @@ contract SlotGame {
         return addressToUser[msg.sender];
     }
 
+    /// @notice Add the number of wins and save the time.
     function countUpWinsCount() public {
         // 作成済みであること
         require(
@@ -61,6 +78,7 @@ contract SlotGame {
         addressToUser[msg.sender].winsCount += 1;
     }
 
+    /// @notice Add the number of loses and save the time.
     function countUpLoseCount() public {
         // 作成済みであること
         require(
